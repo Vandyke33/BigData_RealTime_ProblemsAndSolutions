2b8c21bd3a5de71547870408a87a4fc8473b6fdc
Shubham Patil <66055727+Vandyke33@users.noreply.github.com>
2022-11-02 10:45:20 +0530

    Create solution.py

---
+++ SkipFirstFewRows/solution.py
@@ -0,0 +1,24 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import *
+
+spark = SparkSession.builder.master('local').appName('SkipFirstFewRows').getOrCreate()
+
+df=spark.read.csv("data.csv",header=True)
+df.show()
+
+rdd = sc.textFile("data.csv")
+rdd.collect()
+
+final_rdd = rdd.zipWithIndex().filter(lambda x: x[1] > 2).map(lambda a :a[0].split(","))
+final_rdd.collect()
+
+# get columns into list to create DataFrame
+columns=final_rdd.collect()[0]
+print(columns)
+
+# skip header/columns from data 
+columns=final_rdd.first() #extract header for filter that row
+data = final_rdd.filter(lambda col: col != columns)   #filter out header
+data.collect()
+
+data.toDF(columns).show()
